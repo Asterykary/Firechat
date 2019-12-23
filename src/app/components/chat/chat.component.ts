e3e0de486913d14f4acd0cb8eee1fdadf2b02b93
Karina <k_ale@outlook.com>
2019-12-23 16:11:37 -0300

    Desplegando mensajes de chat correctamente

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -10,19 +10,29 @@ import { ChatService } from '../../providers/chat.service';
 export class ChatComponent implements OnInit {
 
   mensaje = '';
+  elemento: any;
+
   constructor(public cs: ChatService) {
 
     this.cs.cargarMensajes()
-    .subscribe();
+    .subscribe(() => {
+
+      setTimeout(() => {
+        this.elemento.scrollTop = this.elemento.scrollHeight;
+      }, 20);
+
+
+    });
    }
 
   ngOnInit() {
+    this.elemento = document.getElementById('app-mensajes');
   }
 
   enviar_mensaje() {
     console.log(this.mensaje);
 
-    if( this.mensaje.length === 0){
+    if ( this.mensaje.length === 0) {
       return;
     }
 
